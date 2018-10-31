544fe3e6118cf5aadfb3ac73e09e4fdbf1b2f551
Osiris <tim@chaubet.be>
2018-10-31 15:44:27 +0100

    Update grabber.sh

--- grabber.sh
+++ grabber.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ffmpeg -rtsp_transport tcp -i $stream1 -vcodec copy -an -map 0 -f segment -segment_time 15 -segment_format mp4 -strftime 1 "/www/%Y-%m-%d_%H-%M-%S.mp4"
+ffmpeg  -rtsp_transport tcp \
+        -i $stream1 \
+        -vcodec copy \
+        -an \
+        -map 0 \
+        -f segment \
+        -segment_time 15 \
+        -segment_format mp4 \
+        -strftime 1 \
+        "/www/%Y-%m-%d_%H-%M-%S.mp4"
