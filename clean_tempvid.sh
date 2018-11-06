161430f2419db9d17c6c2e5757faf96cf81f7667
Osiris <tim@chaubet.be>
2018-11-06 10:15:34 +0100

    Create clean_tempvid.sh

---
+++ clean_tempvid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Takes an argument like '/www/tempvid/stream1/' including the slashes.
+# This will constantly keep only the most recent 20 files in folder /www/tempvid/stream1/
+# 
+# \ls -tp /www/tempvid/stream1/ | grep -v '/$' | tail -n +21 | xargs -I {} rm -- "/www/tempvid/stream1/{}"
+#
+
+while :
+        do
+                \ls -tp $1 | grep -v '/$' | tail -n +21 | xargs -I {} rm -- "$1{}"
+done
