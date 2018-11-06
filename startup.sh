32503b5d9e76a168d550e831ffd3d8cf985bc3d5
Osiris <tim@chaubet.be>
2018-11-06 10:04:19 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir "/www/tempvid" 2>&1
+chmod -R 777 /www/tempvid 2>&1
 for y in {1..10}
 do
         s="stream$y";
@@ -8,6 +10,24 @@ do
         else
                 if [ -n "$q" ]; then
                         echo "$s: $q";
+                        mkdir "/www/$s" 2>&1;
+                        chmod -R 777 /www/$s 2>&1;
+                        mkdir "/www/tempvid/$s" 2>&1;
+                        chmod -R 777 /www/tempvid/$s 2>&1;
+                        # ./grabber.sh $q $s
+                        ffmpeg  -rtsp_transport tcp \
+                                -i $q \
+                                -vcodec copy \
+                                -an \
+                                -map 0 \
+                                -f segment \
+                                -segment_time 15 \
+                                -segment_format mp4 \
+                                -strftime 1 \
+                                "/www/tempvid/$s/$s-%Y-%m-%d_%H-%M-%S.mp4" \
+                                >/dev/null 2>&1 &
+                else
+                        echo "$s is not defined.";
                 fi
         fi
 done
