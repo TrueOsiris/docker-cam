692060aff368d338897c16ccbacdadfd0ee80375
Osiris <tim@chaubet.be>
2018-11-06 16:28:17 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir "/www/tempvid" 2>&1
 chmod -R 777 /www/tempvid 2>&1
+mkdir "/www/tempthumb" 2>&1
+chmod -R 777 www/tempthumb 2>&1
 for y in {1..10}
 do
         s="stream$y";
@@ -9,15 +11,21 @@ do
                 echo "$s is unset";
         else
                 if [ -n "$q" ]; then
-                        echo "Starting processes for $s: $q";
+                        echo "$s: $q";
                         mkdir "/www/$s" 2>&1;
                         chmod -R 777 /www/$s 2>&1;
                         mkdir "/www/tempvid/$s" 2>&1;
                         chmod -R 777 /www/tempvid/$s 2>&1;
-                        ps -auxw | grep -e [f]fmpeg | grep $s | awk '{print $2}' | xargs kill 2>/dev/null
-                        ps -auxw | grep -e [c]lean_ | grep $s | awk '{print $2}' | xargs kill 2>/dev/null
+                        mkdir "/www/tempthumb/$s" 2>&1;
+                        chmod -R 777 /www/tempthumb/$s 2>&1;
+                        ps -auxw | grep -e [f]fmpeg | grep $s | grep mp4 | awk '{print $2}' | xargs kill 2>/dev/null;
+                        ps -auxw | grep -e [f]fmpeg | grep $s | grep jpg | awk '{print $2}' | xargs kill 2>/dev/null;
+                        ps -auxw | grep -e [c]lean_ | grep vid | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
+                        ps -auxw | grep -e [c]lean_ | grep thumb | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
+                        # cleaning up extra temp videos and thumbs
                         /sbin/clean_tempvid.sh "/www/tempvid/$s/" &
-
+                        /sbin/clean_tempvid.sh "/www/tempthumb/$s/" &
+                        # getting 15 second video segments
                         ffmpeg  -rtsp_transport tcp \
                                 -i $q \
                                 -vcodec copy \
@@ -29,8 +37,18 @@ do
                                 -strftime 1 \
                                 "/www/tempvid/$s/$s-%Y-%m-%d_%H-%M-%S.mp4" \
                                 >/dev/null 2>&1 &
+                        # getting thumbnails
+                        ffmpeg  -rtsp_transport tcp \
+                                -i $q \
+                                -an \
+                                -map 0 \
+                                -vf fps=1 \
+                                -strftime 1 \
+                                "/www/tempthumb/$s/$s-%Y-%m-%d_%H-%M-%S.jpg" \
+                                >/dev/null 2>&1 &
                 else
                         echo "$s is not defined.";
                 fi
         fi
 done
+
