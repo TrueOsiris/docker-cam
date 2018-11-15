8501ae92b068a0b61d2818f7da0f548ebcd0953d
Osiris <tim@chaubet.be>
2018-11-15 14:38:22 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -19,15 +19,21 @@ do
                         mkdir "/www/tempvid/$s" 2>/dev/null;
                         chmod -R 777 /www/tempvid/$s 2>/dev/null;
                         mkdir "/www/tempthumb/$s" 2>/dev/null;
+                        mkdir "/www/tempthumb/$s/bw" 2>/dev/null;
                         chmod -R 777 /www/tempthumb/$s 2>/dev/null;
+                        echo "stopping ffmpeg environments and scripts for $s ..."
                         ps -auxw | grep -e [f]fmpeg | grep $s | grep mp4 | awk '{print $2}' | xargs kill 2>/dev/null;
                         ps -auxw | grep -e [f]fmpeg | grep $s | grep jpg | awk '{print $2}' | xargs kill 2>/dev/null;
                         ps -auxw | grep -e [c]lean_ | grep vid | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
+                        ps -auxw | grep -e [c]lean_ | grep thumb | grep bw | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
                         ps -auxw | grep -e [c]lean_ | grep thumb | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
                         ps -auxw | grep -e [c]ompare | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
+                        echo "starting everything for $s in 3 seconds ..."
+                        sleep 3
                         # cleaning up extra temp videos and thumbs
                         /sbin/clean_tempvid.sh "/www/tempvid/$s/" &
                         /sbin/clean_tempvid.sh "/www/tempthumb/$s/" &
+                        /sbin/clean_tempvid.sh "/www/tempthumb/$s/bw/" &
                         # getting 15 second video segments
                         ffmpeg  -rtsp_transport tcp \
                                 -i $q \
@@ -40,7 +46,7 @@ do
                                 -strftime 1 \
                                 "/www/tempvid/$s/$s-%Y-%m-%d_%H-%M-%S.mp4" \
                                 >/dev/null 2>&1 &
-                        # getting thumbnails
+                        # getting a pic per second
                         ffmpeg  -rtsp_transport tcp \
                                 -i $q \
                                 -an \
@@ -49,12 +55,17 @@ do
                                 -strftime 1 \
                                 "/www/tempthumb/$s/$s-%Y-%m-%d_%H-%M-%S.jpg" \
                                 >/dev/null 2>&1 &
+                        # getting black and white thumbnails
+                        ffmpeg  -rtsp_transport tcp \
+                                -i $q \
+                                -an \
+                                -map 0 \
+                                -vf fps=1,format=gray,format=yuv422p \
+                                -strftime 1 \
+                                "/www/tempthumb/$s/bw/$s-%Y-%m-%d_%H-%M-%S.jpg" \
+                                >/dev/null 2>&1 &
                         # copying pics with differences
                         /sbin/compare.sh $s &
-
-                else
-                        echo "$s is not defined."
                 fi
         fi
 done
-
