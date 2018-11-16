1015ac0aff805289ddbbbd79072fe10d70d62998
Osiris <tim@chaubet.be>
2018-11-16 13:21:32 +0100

    Create timelapse.sh

---
+++ timelapse.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+while :
+  do
+    for yy in {1..12}
+      do
+        ss="stream$yy";
+        if [ -d "/www/$ss" ]; then
+          basedir="/www/$ss/pics"
+          today=$(date "+%Y-%m-%d")
+          t=`ls -d $basedir/20*/ | awk -F/ '{print$5}'`
+          arr=($t)
+          for i in "${arr[@]}"
+          do
+            if [ $i == $today ]; then
+              ffmpeg -y -framerate 24 -pattern_type glob -i "$basedir/$i/*.jpg" -c:v libx264 -pix_fmt yuv420p \
+                "$basedir/${i}_temp_timelapse.mp4" >/dev/null 2>&1 &
+            else
+              file="$basedir/${i}_timelapse.mp4"
+              if [[ ! -f "$file" ]]; then
+                ffmpeg -framerate 24 -pattern_type glob -i "$basedir/$i/*.jpg" -c:v libx264 -pix_fmt yuv420p \
+                  "$basedir/${i}_timelapse.mp4" >/dev/null 2>&1 &
+              fi
+            fi
+          done
+        fi
+    done
+    sleep 1800
+done
