4db10d872608b358d152ab333892f8b1f3fc357d
Osiris <tim@chaubet.be>
2018-12-20 12:59:07 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -37,52 +37,49 @@ do
                         mkdir "/www/tempthumb/$s" 2>/dev/null;
                         mkdir "/www/tempthumb/$s/bw" 2>/dev/null;
                         chmod -R 777 /www/tempthumb/$s 2>/dev/null;
-                        echo "stopping ffmpeg environments and scripts for $s ..."
-                        ps -auxw | grep -e [f]fmpeg | grep $s | grep mp4 | awk '{print $2}' | xargs kill 2>/dev/null;
-                        ps -auxw | grep -e [f]fmpeg | grep $s | grep jpg | awk '{print $2}' | xargs kill 2>/dev/null;
-                        ps -auxw | grep -e [c]lean_ | grep vid | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
-                        ps -auxw | grep -e [c]lean_ | grep thumb | grep bw | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
-                        ps -auxw | grep -e [c]lean_ | grep thumb | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
-                        ps -auxw | grep -e [c]ompare | grep $s | awk '{print $2}' | xargs kill 2>/dev/null;
-                        ps -auxw | grep -e [t]imelapse | awk '{print $2}' | xargs kill 2>/dev/null;
-                        ps -auxw | grep sleep | awk '{print $2}' | xargs kill 2>/dev/null;
+                        echo "stopping ffmpeg environments and scripts for $s ...";
+                        t=`ps -auxw | grep $s | grep blackwhite | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping blackwhite thumb generation healthcheck process for $s: $t";
+                        ps -auxw | grep $s | grep blackwhite | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep $s | grep capturepics | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping picture generation healthcheck process for $s: $t";
+                        ps -auxw | grep $s | grep capturepics | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep $s | grep segments | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping mp4 segment generation healthcheck process for $s: $t";
+                        ps -auxw | grep $s | grep segments | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep -e [f]fmpeg | grep $s | grep mp4 | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping mp4 generation process for $s: $t";
+                        ps -auxw | grep -e [f]fmpeg | grep $s | grep mp4 | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep -e [f]fmpeg | grep $s | grep jpg | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping picture capturing process for $s: $t";
+                        ps -auxw | grep -e [f]fmpeg | grep $s | grep jpg | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep -e [c]lean_ | grep vid | grep $s | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping cleanup process for thumbvids for $s: $t";
+                        ps -auxw | grep -e [c]lean_ | grep vid | grep $s | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep -e [c]lean_ | grep thumb | grep bw | grep $s | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping cleaning process for blackwhite thumbs for $s: $t";
+                        ps -auxw | grep -e [c]lean_ | grep thumb | grep bw | grep $s | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep -e [c]lean_ | grep thumb | grep $s | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping cleaning process for pictures for $s: $t";
+                        ps -auxw | grep -e [c]lean_ | grep thumb | grep $s | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep -e [c]ompare | grep $s | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping comparison process for $s: $t";
+                        ps -auxw | grep -e [c]ompare | grep $s | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep -e [t]imelapse | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping timelapse processes: $t";
+                        ps -auxw | grep -e [t]imelapse | awk '{print $2}' | xargs kill -9 2>/dev/null;
+                        t=`ps -auxw | grep sleep | awk '{print $2}' 2>/dev/null;`;
+                        echo "stopping sleep processes: $t";
+                        ps -auxw | grep sleep | awk '{print $2}' | xargs kill -9 2>/dev/null;
                         echo "starting everything for $s in 3 seconds ..."
                         sleep 3
                         # cleaning up extra temp videos and thumbs
                         /sbin/clean_tempvid.sh "/www/tempvid/$s/" &
                         /sbin/clean_tempvid.sh "/www/tempthumb/$s/" &
                         /sbin/clean_tempvid.sh "/www/tempthumb/$s/bw/" &
-                        # getting 15 second video segments
-                        ffmpeg  -rtsp_transport tcp \
-                                -i $q \
-                                -vcodec copy \
-                                -an \
-                                -map 0 \
-                                -f segment \
-                                -segment_time 15 \
-                                -segment_format mp4 \
-                                -strftime 1 \
-                                "/www/tempvid/$s/$s-%Y-%m-%d__%H-%M-%S.mp4" \
-                                >/dev/null 2>&1 &
-                        # getting a pic per second
-                        ffmpeg  -rtsp_transport tcp \
-                                -i $q \
-                                -an \
-                                -map 0 \
-                                -vf fps=1 \
-                                -strftime 1 \
-                                "/www/tempthumb/$s/$s-%Y-%m-%d__%H-%M-%S.jpg" \
-                                >/dev/null 2>&1 &
-                        # getting black and white thumbnails
-                        ffmpeg  -rtsp_transport tcp \
-                                -i $q \
-                                -an \
-                                -map 0 \
-                                -vf fps=1,format=gray,format=yuv422p,scale="512:-1" \
-                                -strftime 1 \
-                                "/www/tempthumb/$s/bw/$s-%Y-%m-%d__%H-%M-%S.jpg" \
-                                >/dev/null 2>&1 &
-                        # copying pics with differences
+                        /sbin/segments.sh "$s" "$q" &
+                        /sbin/capturepics.sh "$s" "$q" &
+                        /sbin/blackwhite.sh "$s" "$q" &
                         /sbin/compare.sh $s "$fuz" "pics" &
                         #/sbin/compare.sh $s 8 "pics/fuz8" &
                 fi
