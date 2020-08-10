7ed42cc7af6f9cee7a3836d7a1ed9f6612f5890c
Osiris <tim@chaubet.be>
2020-08-10 12:34:28 +0200

    Update timelapse.sh

--- timelapse.sh
+++ timelapse.sh
@@ -6,13 +6,13 @@ while :
         ss="stream$yy";
         if [ -d "/www/$ss" ]; then
           basedir="/www/$ss/pics"
+          find $basedir/ -iname "*.log" -type f -mtime +15 -exec rm -f {} +
           today=$(date "+%Y-%m-%d")
           t=`ls -d $basedir/20*/ | awk -F/ '{print$5}'`
           arr=($t)
           for i in "${arr[@]}"
           do
             find $basedir/$i/ -type f -size 0 -delete
-            find $basedir/ -iname "*.log" -type f -mtime +15 -exec rm -f {} +
             if [ $i == $today ]; then
               ffmpeg -y -framerate 24 -pattern_type glob -i "$basedir/$i/*.jpg" -c:v libx264 -pix_fmt yuv420p \
                 "$basedir/${i}_temp_timelapse_in_progress.mp4" >/dev/null 2>&1
